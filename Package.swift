3e81c0d5694c430d1ccd607f3b78f3c6eaae62bb
Adrian Zimmermann <zadrian@web.de>
2021-05-22 11:30:54 +0200

    update to swift 5

--- Package.swift
+++ Package.swift
@@ -1,9 +1,26 @@
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
     name: "Nifty-libs",
-    providers: [
-        .Apt("gfortran liblapack3 liblapacke liblapacke-dev libopenblas-base libopenblas-dev"),
-        .Brew("homebrew/dupes/lapack homebrew/science/openblas")
+    products: [
+        .library(name: "CLapacke", targets: ["CLapacke"]),
+        .library(name: "CBlas", targets: ["CBlas"])
+    ],
+    targets: [
+        .systemLibrary(
+            name: "CLapacke",
+            providers: [
+                .apt(["gfortran liblapack3", "liblapacke", "liblapacke-dev"]),
+                .brew(["homebrew/dupes/lapack"])
+            ]
+        ),
+        .systemLibrary(
+            name: "CBlas",
+            providers: [
+                .apt(["libopenblas-base", "libopenblas-dev"]),
+                .brew(["homebrew/science/openblas"])
+            ]
+        )
     ]
 )
